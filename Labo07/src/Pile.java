3b4f8b13ee40c86d9510de06d39cc8c93c2371c2
Robin Müller <rm.muller@bluewin.com>
2019-11-15 14:00:24 +0100

    Pile

---
+++ Labo07/src/Pile.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Pile {
+    private LinkedList<Object> elements;
+
+    public Pile() {
+        this.elements = new LinkedList<>();
+    }
+
+    public Pile(LinkedList<Object> elements) {
+        //Implement deep copy
+        this.elements = new LinkedList<>(elements);
+    }
+
+    public void push(Object element) {
+        elements.push(element);
+    }
+
+    public Object pop() {
+        return elements.pop();
+    }
+
+    public LinkedList<Object> getElements() {
+        return elements;
+    }
+
+    public Iterator<Object> iterator() {
+        return elements.iterator();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+        for (Object el : elements) {
+            sb.append(" <").append(el).append(">");
+        }
+        sb.append(" ]");
+
+        return sb.toString();
+    }
+}
