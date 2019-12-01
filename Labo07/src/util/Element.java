d0be821bffe53d64bb5ff4bed316def38c5d1ef3
Naludrag <stephanecarvalho81@gmail.com>
2019-12-01 20:27:16 +0100

    Debut

---
+++ Labo07/src/util/Element.java
@@ -0,0 +1,28 @@
+package util;
+
+/**
+ * Classe permettant la gestion d'élément de type Object dans une pile
+ * @author Müller Robin, Teixeira Carvalho Stéphane
+ */
+class Element {
+    Object data;
+    Element next;
+
+    /**
+     * Constructeur de la classe Element
+     * @param data object répresentant la valeur de l'élément
+     */
+    Element(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * Deuxième constructeur de la classe object permettant d'ajouter un élément en depuis de pile
+     * @param data object représentant la valeur de l'élément
+     * @param next objet représentant la valeur de l'élément suivant dans la pile
+     */
+    Element(Object data, Element next) {
+        this(data);
+        this.next = next;
+    }
+}
--- Labo07/src/util/Pile.java
+++ Labo07/src/util/Pile.java
@@ -1,30 +1,4 @@
-package util;
-/**
- * Classe permettant la gestion d'élément de type Object dans une pile
- * @author Müller Robin, Teixeira Carvalho Stéphane
- */
-class Element {
-    Object data;
-    Element next;
-
-    /**
-     * Constructeur de la classe Element
-     * @param data object répresentant la valeur de l'élément
-     */
-    Element(Object data) {
-        this.data = data;
-    }
-
-    /**
-     * Deuxième constructeur de la classe object permettant d'ajouter un élément en depuis de pile
-     * @param data object représentant la valeur de l'élément
-     * @param next objet représentant la valeur de l'élément suivant dans la pile
-     */
-    Element(Object data, Element next) {
-        this(data);
-        this.next = next;
-    }
-}
+package util;//permet d'inclure les classes Element et Pile dans la package util
 /**
  * Classe permettant de représenter une pile générique (capable de stocker un nombre quelconque
  * d’objets)
