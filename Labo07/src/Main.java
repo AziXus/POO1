025c819606e1a776ffda5fdd967e7b45d488bc61
Naludrag <stephanecarvalho81@gmail.com>
2019-12-02 11:55:15 +0100

    Debut

--- Labo07/src/Main.java
+++ Labo07/src/Main.java
@@ -7,22 +7,8 @@ import util.Pile;
 public class Main {
 
     public static void main(String[] args) {
-        Pile list = new Pile();
-
-        list.push("A");
-        list.push("B");
-        list.push("C");
-        list.push("D");
-        list.push("E");
-
-        // ListIterator to traverse the list
-        Iterateur iterator = list.iterateur();
-
-        // Traversing the list in forward direction
-        System.out.println("Displaying list elements : ");
-
-        while (iterator.possedeSuivant())
-            System.out.print(iterator.suivant() + " ");
+        testPile(4);
+        System.out.println("Test lancment Hanoi");
         if(args.length == 0){
             testHanoiApplication();
         }
@@ -34,6 +20,54 @@ public class Main {
                 System.out.println("La valeur passée en paramètre n'est pas un nombre");
             }
         }
+    }
+
+    /**
+     * Test le programme de résolution de tour de hanoi en invite de commande
+     * @param nbElement nombre de disque qu'aura la tour de hanoi
+     */
+    private static void testPile(int nbElement) {
+        Pile p = new Pile();
+        System.out.println("Pile vide ? " + p.isEmpty());
+        System.out.println("Taille    : " + p.size());
+        for(int i = 0; i < nbElement; i++){
+            System.out.println("Ajout de l'élément : " + i);
+            p.push(i);
+        }
+        System.out.println("Pile vide ? " + p.isEmpty());
+        System.out.println("Taille    : " + p.size());
+
+        System.out.println("Affichage des éléments de la pile sous forme du tableau : ");
+        Object[] elements = p.getElements();
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for(int i = 0; i < elements.length; i++){
+            sb.append(elements[i] + " ");
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+
+        System.out.println("Supression d'un élément : " + p.pop());
+        System.out.println("Taille    : " + p.size());
+
+        System.out.println("Affichage des éléments de la pile sous forme du tableau : ");
+        elements = p.getElements();
+        sb = new StringBuilder();
+        sb.append("[ ");
+        for(int i = 0; i < elements.length; i++){
+            sb.append(elements[i] + " ");
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+
+        System.out.println("Parcours de la pile avec un Iterateur");
+        Iterateur it = p.iterateur();
+        sb = new StringBuilder();
+        while(it.possedeSuivant())
+            sb.append(it.suivant() + " ");
+        System.out.println(sb.toString());
+
+        System.out.println(p.toString());
     }
 
     /**
